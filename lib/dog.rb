4c6ecb378bf7089112f7b77e4d459aa0bb01e496
Kelsey Fecho <kfecho30@gmail.com>
2018-03-05 02:36:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,18 @@
 class Dog
-  def initialize(name, breed = "Mutt")
+  def initialize(name)
     @name = name
-    @breed = breed
   end
 
   def name
     @name
   end
 
-  def breed
-    @breed
-  end
-
   def name=(new_name)
     @name = new_name
   end
 
-  def breed=(new_breed)
-    @breed = new_breed
+  def bark
+    puts "Woof!"
   end
+
 end
